15e4ec5fd912f7f8011ac7fb6834ec4fa9bce737
Akshaya <akshayasbramanyam8@gmail.com>
2021-03-13 17:30:28 +0530

    init

---
+++ src/test/java/com/example4/example4/ReverseArray.java
@@ -0,0 +1,11 @@
+package com.example4.example4;
+
+public class ReverseArray
+{
+	public static void main(String[] args)
+	{
+		int arr=[1 2 3 4 5]
+		
+	}
+
+}
---
+++ src/test/java/com/example4/example4/TestClass2.java
@@ -0,0 +1,20 @@
+package com.example4.example4;
+
+import static io.restassured.RestAssured.given;
+
+import java.io.File;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+public class TestClass2 {
+	@Test
+	public void test12()
+	{
+
+		
+		
+		
+	}
+}
